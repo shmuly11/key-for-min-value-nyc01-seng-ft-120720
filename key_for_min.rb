51651c33b5039c2192454c7b8adbaf2fd7ab23c6
shmuly levitin <shmuly11@gmail.com>
2020-11-23 02:42:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+lowest = 1000
+name_hash.each do |key, value|
+  if value < lowest
+    lowest = value
+    pop = key 
+  end
+  pop
 end
